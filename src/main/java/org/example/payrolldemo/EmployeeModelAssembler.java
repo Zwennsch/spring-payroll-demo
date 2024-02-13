4cae8463452c2e84ccfed8d66e1bd905a49cfdd6
Sven Schroeder <schuen@gmx.de>
2024-02-13 12:09:28 +0100

    made assembler @Component

--- src/main/java/org/example/payrolldemo/EmployeeModelAssembler.java
+++ src/main/java/org/example/payrolldemo/EmployeeModelAssembler.java
@@ -5,7 +5,9 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>>{
 
     @Override
